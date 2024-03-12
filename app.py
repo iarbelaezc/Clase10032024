a715d1d7bece16702eb009aec4e12e767bfec48e
iarbelaezc <163176799+iarbelaezc@users.noreply.github.com>
2024-03-12 13:36:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,8 @@ import streamlit as st
 from PIL import Image 
 
 st.title(" Aplicación para ciudades inteligentes")
+
+st.header( "En este espacio podrás obtener información de tu ciudad.")
+
+image= Image.open ('1540.jpg')
+st.image(image,caption= 'Inteligencia Urbana')

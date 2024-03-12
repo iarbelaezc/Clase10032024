750e91b1fd7b3f78b688cddf9d0f6b116af08f3e
iarbelaezc <163176799+iarbelaezc@users.noreply.github.com>
2024-03-12 13:40:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ st.title(" Aplicación para ciudades inteligentes")
 
 st.header( "En este espacio podrás obtener información de tu ciudad.")
 
-image= Image.open ('1540.jpg')
+image= Image.open ('imagen.jpg')
 st.image(image,caption= 'Inteligencia Urbana')
